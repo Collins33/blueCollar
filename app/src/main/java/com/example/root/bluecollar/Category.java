f4dfa4ddb1651ce3bb5d87e3b84383feb590aad1
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-22 12:16:24 +0300

    I have added the model for category class and added a recyclerview as a dependency

---
+++ app/src/main/java/com/example/root/bluecollar/Category.java
@@ -0,0 +1,21 @@
+package com.example.root.bluecollar;
+
+/**
+ * Created by root on 9/22/17.
+ */
+
+public class Category {
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
