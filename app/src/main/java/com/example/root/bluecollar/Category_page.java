371a6e8555b6e72b0e18791e0594cbac4915f1fd
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-22 12:07:27 +0300

    I have added the layout to show all categories of jobs available and added a cardview widget

---
+++ app/src/main/java/com/example/root/bluecollar/Category_page.java
@@ -0,0 +1,13 @@
+package com.example.root.bluecollar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Category_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_page);
+    }
+}
