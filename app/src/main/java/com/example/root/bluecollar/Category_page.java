a11ddfe7b946af40e56f898733f74dd6f0e46604
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-22 14:44:53 +0300

    I have added the class to contain my data like the names of categories

--- app/src/main/java/com/example/root/bluecollar/Category_page.java
+++ app/src/main/java/com/example/root/bluecollar/Category_page.java
@@ -3,13 +3,21 @@ package com.example.root.bluecollar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Category_page extends AppCompatActivity {
     private CardView mCardView;
     private TextView mTextView;
-    private String[] categories={"CLEANING","HANDYMAN","LANDSCAPING","DELIVERY","ERRANDS","TUTOR"};
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<Category> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,18 @@ public class Category_page extends AppCompatActivity {
         setContentView(R.layout.activity_category_page);
 
         mCardView=(CardView) findViewById(R.id.card_view);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data=new ArrayList<Category>();
+        for(int i=0; i<=6; i++){
+            data.add(new Category(MyData.categories[i]));
+        }
 
 
 
---
+++ app/src/main/java/com/example/root/bluecollar/MyData.java
@@ -0,0 +1,9 @@
+package com.example.root.bluecollar;
+
+/**
+ * Created by root on 9/22/17.
+ */
+
+public class MyData {
+static String[] categories={"CLEANING","TRANSPORT","LANDSCAPING","DELIVERY","COOKING","HANDYMAN"};
+}
