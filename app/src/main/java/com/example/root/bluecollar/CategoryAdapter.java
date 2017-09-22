f126d3d7c1df675db417f206b069989d0ef4d794
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-22 13:00:21 +0300

    I have added the layout to contain the cards and the recyclerview

---
+++ app/src/main/java/com/example/root/bluecollar/CategoryAdapter.java
@@ -0,0 +1,8 @@
+package com.example.root.bluecollar;
+
+/**
+ * Created by root on 9/22/17.
+ */
+
+public class CategoryAdapter {
+}
--- app/src/main/java/com/example/root/bluecollar/Category_page.java
+++ app/src/main/java/com/example/root/bluecollar/Category_page.java
@@ -2,12 +2,24 @@ package com.example.root.bluecollar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class Category_page extends AppCompatActivity {
+    private CardView mCardView;
+    private TextView mTextView;
+    private String[] categories={"CLEANING","HANDYMAN","LANDSCAPING","DELIVERY","ERRANDS","TUTOR"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_page);
+
+        mCardView=(CardView) findViewById(R.id.card_view);
+
+
+
+
     }
 }
