76017c1b73f4cb2a6f7b34caa3f41efb5b0fe5b1
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-02 18:06:06 +0300

    I have added the method to log a user out and the menu to display the option and inflated it in the category_list activity

--- app/src/main/java/com/example/root/bluecollar/Category_page.java
+++ app/src/main/java/com/example/root/bluecollar/Category_page.java
@@ -1,15 +1,21 @@
 package com.example.root.bluecollar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.root.bluecollar.adapters.CategoryAdapter;
 import com.example.root.bluecollar.models.Category;
+import com.example.root.bluecollar.ui.StartPage;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -43,7 +49,30 @@ public class Category_page extends AppCompatActivity {
            adapter=new CategoryAdapter(data);
            recyclerView.setAdapter(adapter);
 
+    }
+    //inflate the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
 
-
+    }
+    //instruct to do when a menu option is selected
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+        if(id==R.id.logOut){
+            logOut();
+            return true;
+        }
+        return false;
+    }
+    //log user out
+    public void logOut(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent=new Intent(this, StartPage.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
