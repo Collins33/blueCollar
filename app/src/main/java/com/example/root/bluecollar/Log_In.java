e476fbb0ef0379a6ce93f59ea9bdffc237b55869
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-02 16:15:05 +0300

    I have added ability to navigate from login activity to signup activity

--- app/src/main/java/com/example/root/bluecollar/Log_In.java
+++ app/src/main/java/com/example/root/bluecollar/Log_In.java
@@ -1,13 +1,20 @@
 package com.example.root.bluecollar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Log_In extends AppCompatActivity {
-
+     TextView createAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log__in);
+    }
+    public void createAccount(View view){
+        Intent intent=new Intent(getApplicationContext(),Sign_Up.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/root/bluecollar/Sign_Up.java
+++ app/src/main/java/com/example/root/bluecollar/Sign_Up.java
@@ -1,7 +1,9 @@
 package com.example.root.bluecollar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sign_Up extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Sign_Up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__up);
+    }
+    public void signUp(View view){
+        Intent intent=new Intent(getApplicationContext(),Log_In.class);
+        startActivity(intent);
     }
 }
