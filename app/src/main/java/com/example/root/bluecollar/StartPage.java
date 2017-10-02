7789484f39467279a10b064ed25ef0f45f3e770b
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-02 16:07:43 +0300

    I have added the activity that will be first to display

---
+++ app/src/main/java/com/example/root/bluecollar/StartPage.java
@@ -0,0 +1,23 @@
+package com.example.root.bluecollar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+    public void logIn(View view){
+        Intent intent=new Intent(getApplicationContext(),Log_In.class);
+        startActivity(intent);
+    }
+    public void SignUp(View view){
+        Intent intent=new Intent(getApplicationContext(),Sign_Up.class);
+        startActivity(intent);
+    }
+}
