f78ddb3309ce7d4c3cfd8453aed3e47e42ab8479
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-14 21:47:20 +0300

    I have updated the layout of job categories layout

--- app/src/main/java/com/example/root/bluecollar/Category_page.java
+++ app/src/main/java/com/example/root/bluecollar/Category_page.java
@@ -1,13 +1,18 @@
 package com.example.root.bluecollar;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -39,6 +44,7 @@ public class Category_page extends AppCompatActivity implements View.OnClickList
     private static RecyclerView recyclerView;
     private static ArrayList<Category> data;
     private GoogleApiClient mGoogleApiClient;
+    TextView categoryHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +54,10 @@ public class Category_page extends AppCompatActivity implements View.OnClickList
                 .setTitle("WELCOME")
                 .setMessage("CLICK CATEGORY TO ADD TASK")
                 .show();
+        //set font style
+        categoryHeading=(TextView) findViewById(R.id.jobCategories);
+        Typeface song=Typeface.createFromAsset(getAssets(),"fonts/song.ttf");
+        categoryHeading.setTypeface(song);
 
         mCardView=(CardView) findViewById(R.id.card_view);
         //mCardView.setOnClickListener(this);
@@ -55,8 +65,9 @@ public class Category_page extends AppCompatActivity implements View.OnClickList
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
 
-        layoutManager = new LinearLayoutManager(this);
+        layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         data=new ArrayList<Category>();
@@ -114,6 +125,47 @@ public class Category_page extends AppCompatActivity implements View.OnClickList
         Intent intent=new Intent(getApplicationContext(),JobBoard.class);
         startActivity(intent);
     }
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
 
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
 
 }
